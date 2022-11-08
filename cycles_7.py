372420e88f3daea4707748dabe6477eb2e6e3c22
Амалия Зарывных <39524582+amaliyazar@users.noreply.github.com>
2022-11-08 20:11:40 +0300

    Delete cycles_7.py

--- cycles_7.py
+++
@@ -1,8 +0,0 @@
-a=input()
-for i in range(1, len(a), 2):
-    if a[i]!="п" and a[i]!="и" and a[i]!="о":
-        print(a[i], end=" ")
-print()
-for i in range(0, len(a), 2):
-    if a[i]!="п" and a[i]!="и" and a[i]!="о":
-        print(a[i], end=" ")
